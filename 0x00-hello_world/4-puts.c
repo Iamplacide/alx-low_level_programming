08fe32649b26516df3afeced78e701858a6bee27
Placide <placidewiam@gmail.com>
2023-05-22 16:19:56 -0700

    The console print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 /*
- * main - a program that printout a text
- * Return : 0 sucessful
+ * main - a program that printout a text using the puts function
+ * Return: 0 sucessful
  */
-int main()
+main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
