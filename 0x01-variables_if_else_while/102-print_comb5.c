4836af8b61f16b996d8339fecb3d8d93e68f11d0
Placide <placidewiam@gmail.com>
2023-05-22 23:54:39 -0700

    The last comb

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - A program that prints all possible combinaison
+ * of two grouped digits using putchar
+ * Return: 0 always
+ */
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 9 ; a++)
+	{
+		for (b = 0; b < 9; b++)
+		{
+			for (c = 0; c < 10; c++)
+			{
+				for (d = 0; d < 10; d++)
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(' ');
+					putchar(c + '0');
+					putchar(d + '0');
+					if (!(a == 9 && b == 8 && c == 9 && d == 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
