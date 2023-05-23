25cf9efaae8db023858a6b280e4a614a7f311de7
Placide <placidewiam@gmail.com>
2023-05-23 16:16:47 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,19 @@
 #include <stdio>
+#include <main.h>
 /**
  * main - a program that prints a text to the console
  * Return: 0 always
  */
 int main(void)
 {
-	printf("_putchar\n");
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 	return (0);
 }
