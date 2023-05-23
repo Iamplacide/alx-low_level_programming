1705e5eb81363a3a720c4d30260b4190a3f6248b
Placide <placidewiam@gmail.com>
2023-05-22 22:58:43 -0700

    Three digit combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 /**
  * main - A program that prints all possible combinaison
- * of two digits using putchar
+ * of three digits using putchar
  * Return: 0 always
  */
 int main(void)
 {
-	int i, j;
+	int i, j, k;
 
-	for (i = 0; i <= 9 ; i++)
+	for (i = 0; i < 8 ; i++)
 	{
-		for (j = i + 1; j <= 9; j++)
+		for (j = i + 1; j < 9; j++)
 		{
-		putchar(i + '0');
-		putchar(j + '0');
-		if (!(i == 8 && j == 9))
-		{
-		putchar(',');
-		putchar(' ');
-		}
+			for (k = j + 1; k < 10; k++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				if (!(i == 7 && j == 8 && k == 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 	putchar('\n');
