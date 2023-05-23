2d021ca1a7eb0e6e6a137064c3c8e1baef1810c3
Placide <placidewiam@gmail.com>
2023-05-22 17:00:59 -0700

    The last quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,13 @@
 
 /**
  * main - a program that prints a text
- * Return : 1 when successful
+ * Return: 1 when successful
  */
 int main(void)
 {
-const char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-	
+const char *text;
+*text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+
 fputs(text, stdout);
 return (1);
 }
