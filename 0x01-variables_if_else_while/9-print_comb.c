94a40e6c5a977198b482442b5710d08b831eeae1
Placide <placidewiam@gmail.com>
2023-05-22 21:11:36 -0700

    Number + comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - A program that prints numbers + a comma using putchar
+ * Return: 0 always
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
