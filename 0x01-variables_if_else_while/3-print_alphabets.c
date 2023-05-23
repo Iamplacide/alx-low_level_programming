a6d9d1ee9070ac4f59ae1e6b75304e8f188e78a9
Placide <placidewiam@gmail.com>
2023-05-22 20:10:32 -0700

    Upper and lowercase alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - A program that prints lowercase and uppercase alphabet using putchar
+ * Return: 0 always
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
