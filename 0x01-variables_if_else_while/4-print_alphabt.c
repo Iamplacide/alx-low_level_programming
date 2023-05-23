3a1f1c70b8a9fe300e904c0348e2d5bf7844ff4c
Placide <placidewiam@gmail.com>
2023-05-22 20:31:11 -0700

    Omit q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - A program that prints alphabet using putchar except q and e
+ * Return: 0 always
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != 'q' && letter != 'e')
+		{
+			putchar(letter);
+		}
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
