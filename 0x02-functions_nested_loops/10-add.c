2106199cbc50b07b5726fd9ff7040c55d9ff4b86
Placide <placidewiam@gmail.com>
2023-05-24 18:30:22 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add -  a program that adds two integers
+ * @n: the first integer
+ * @m: the second integer
+ * Return: the result of the addition
+ */
+int add(int n, int m)
+{
+	int res;
+
+	res = (n + m);
+	return (res);
+}
