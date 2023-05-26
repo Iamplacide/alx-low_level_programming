52a55166410eec0c1b2f421df46efd49b3138d95
Placide <placidewiam@gmail.com>
2023-05-26 16:51:40 -0700

    029

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers from 0 to 9;
+ * Return: 0 always
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
