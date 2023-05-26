c030fc1bb51934a273d98edec2e304108dc41cfd
Placide <placidewiam@gmail.com>
2023-05-26 16:39:38 -0700

    The mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - a program that multiplies two numbers
+ * @a: the first number
+ * @b: the second number
+ * Return: the value of that mul always
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
