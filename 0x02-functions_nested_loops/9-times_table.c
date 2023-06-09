1898a0366d262f90fbbcaa1606178d7fa376e3be
Placide <placidewiam@gmail.com>
2023-06-09 04:33:34 -0700

    Times tables zero to nine

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * times_table - a program that prints the nine times table starting with zero
+ * Return: 0 always
+ */
+void times_table(void)
+{
+	int a, b, mul;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			mul = a * b;
+			if (mul < 10)
+			{
+				_putchar(' ');
+				_putchar(mul + '0');
+			}
+			if (mul >= 10)
+			{
+				_putchar((mul / 10) + '0');
+				_putchar((mul % 10) + '0');
+			}
+			if (b != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
