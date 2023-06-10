67c678d140c0457a544cb032f5662e648df57c69
Placide <placidewiam@gmail.com>
2023-06-10 14:27:49 -0700

    To ninety eight

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * print_to_98 - Prints all numbers to ninety-height.
+ * @n: the beginning position.
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
