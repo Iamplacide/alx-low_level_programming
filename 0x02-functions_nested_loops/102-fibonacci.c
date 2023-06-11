5ffcce9fdea41d23e0470edebfcbe33fb4762ad5
Placide <placidewiam@gmail.com>
2023-06-11 10:53:02 -0700

    The 50 Fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - a program that prints the first Fibonacci numbers
+ * Return: 0 always
+ */
+int main(void)
+{
+	int c;
+	unsigned long a, b, sum;
+
+	a = 0;
+	b = 1;
+	for (c = 1; c <= 50; c++)
+	{
+		sum = a + b;
+		a = b;
+		b = sum;
+		printf("%ld, ", sum);
+	}
+	putchar('\n');
+	return (0);
+}
