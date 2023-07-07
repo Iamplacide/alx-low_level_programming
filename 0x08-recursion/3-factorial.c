6c6e54ed2f80efbe6ab7fb8e34ded571f9b535b0
Placide <placidewiam@gmail.com>
2023-07-07 10:00:09 -0700

    Factorial n 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * factorial -  a program that returns the factorial of a digit
+ * using a recursion
+ * @n: The digit to compute the factorial
+ * Return: The factorial in digits.
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n*factorial(n - 1));
+	}
+}
