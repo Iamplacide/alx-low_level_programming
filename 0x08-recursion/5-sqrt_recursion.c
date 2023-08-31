782e516ad15f583570cac466eff7e882ba396bda
placide <placidewiam@gmail.com>
2023-08-30 19:19:07 -0700

    The square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,37 +1,46 @@
 #include "main.h"
 
+int search(int num, int root);
+int _sqrt_recursion(int n);
+
+/**
+ * search - finds the square root using recursion
+ * @num: number taken form the input
+ * @root: the square root number to find
+ * Return: The result if it has a whole number root,
+ * else it returns -1
+ */
+
+int search(int num, int root)
+{
+	if ((root * root) == num)
+	{
+		return (root);
+	}
+
+	if (root == num / 2)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (search(num, root + 1));
+	}
+}
+
 /**
  * _sqrt_recursion -  a program that finds the
  * whole square root of a function
  * @n: the given number
  * Return: The square root solution.
  */
-int _sqrt_recursion(int n)
+	int _sqrt_recursion(int n)
 {
-	int root;
+	int root = 0;
 
-	if (n == 0)
-	{
+	if (n <= 0)
 		return (-1);
-	}
 	else if (n == 1)
-	{
 		return (1);
-	}
-
-	root = (n / 2);
-
-	while ((root * root) != n && root != 0)
-	{
-		root--;
-	}
-
-	if (root == 0)
-	{
-		return (-1);
-	}
-	else
-	{
-		return (root);
-	}
+	return (search(n, root));
 }
