dbd8a0b973d3f065e9a44d6daec6bd642630285d
placide <placidewiam@gmail.com>
2023-09-11 19:30:46 -0700

    Struct and type def

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Welcome to my struct and typedef directory. Let's go
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * struct dog - a struc recording everything about a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	double age;
+	char *owner;
+};
